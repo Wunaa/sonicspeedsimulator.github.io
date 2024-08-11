5808af40df02736408c686b6fc9782e889f2b437
Wunaa <104600804+Wunaa@users.noreply.github.com>
2024-08-10 18:02:12 -0600

    Update script.js

--- discord/script.js
+++ discord/script.js
@@ -5,17 +5,3 @@ function openPopup(popupId) {
 function closePopup(popupId) {
     document.getElementById(popupId).style.display = 'none';
 }
-
-function shareAwareness() {
-    const shareData = {
-        title: 'Discord Scam Awareness',
-        text: 'Learn how to stay safe from Discord scams. Visit our website for detailed information.',
-        url: window.location.href
-    };
-
-    if (navigator.share) {
-        navigator.share(shareData).catch(console.error);
-    } else {
-        alert("Sharing is not supported on this browser.");
-    }
-}
